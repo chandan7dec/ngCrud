2996e7deeb3e1dc8d839a12387c52cff31f47225
chandan7dec <ck.kislay@gmail.com>
2018-01-20 18:35:58 +0530

    Added ngx-bootstrap date control

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Department } from '../models/department.model';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-create-employee',
@@ -11,7 +12,9 @@ export class CreateEmployeeComponent implements OnInit {
   // gender= 'male';
   // cotactPreference= 'email';
   // isActive = true;
-  // department = 3;{
+  // department = 3;
+
+  datePickerConfig: Partial<BsDatepickerConfig>;
 
     departments: Department[] = [
     { id: 1, name: 'Help Desk'},
@@ -19,7 +22,16 @@ export class CreateEmployeeComponent implements OnInit {
     { id: 3, name: 'HR'},
     { id: 4, name: 'Paroll'},
   ];
-  constructor() { }
+  constructor() {
+    this.datePickerConfig = Object.assign({},
+      {
+      containerClass: 'theme-dark-blue',
+      showWeekNumbers: true,
+      minDate: new Date(2018, 0, 1),
+      maxDate: new Date(2018, 11, 31),
+      dateInputFormat: 'DD/MM/YYYY'
+      });
+   }
 
   ngOnInit() {
   }
