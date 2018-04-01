9b652649e5a7da41c9a7e24a8e15123ef42cec10
chandan7dec <ck.kislay@gmail.com>
2018-04-01 14:31:35 +0530

    Tutorial 30,31,32

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { AppComponent } from './app.component';
 import { ListEmployeesComponent } from './employees/list-employees.component';
 import { CreateEmployeeComponent } from './employees/create-employee.component';
+import { EmployeeService } from './employees/employee.service';
 
 
 const appRoutes: Routes = [
@@ -27,7 +28,7 @@ const appRoutes: Routes = [
     BsDatepickerModule.forRoot(),
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Department } from '../models/department.model';
-import {Employee} from '../models/employee.model';
+import { Employee } from '../models/employee.model';
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+import { EmployeeService } from './employee.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-employee',
@@ -24,37 +26,39 @@ export class CreateEmployeeComponent implements OnInit {
     phoneNumber: null,
     email: '',
     dateOfBirth: null,
-    department: null,
+    department: '-1',
     isActive: null,
     photoPath: null
 
   };
-    departments: Department[] = [
-    { id: 1, name: 'Help Desk'},
-    { id: 2, name: 'IT'},
-    { id: 3, name: 'HR'},
-    { id: 4, name: 'Paroll'},
+  departments: Department[] = [
+    { id: 1, name: 'Help Desk' },
+    { id: 2, name: 'IT' },
+    { id: 3, name: 'HR' },
+    { id: 4, name: 'Paroll' },
   ];
-  constructor() {
+  constructor(private _employeeService: EmployeeService, private _router: Router) {
     this.datePickerConfig = Object.assign({},
       {
-      containerClass: 'theme-dark-blue',
-      showWeekNumbers: true,
-      minDate: new Date(2018, 0, 1),
-      maxDate: new Date(2018, 11, 31),
-      dateInputFormat: 'DD/MM/YYYY'
+        containerClass: 'theme-dark-blue',
+        showWeekNumbers: true,
+        minDate: new Date(2018, 0, 1),
+        maxDate: new Date(2018, 11, 31),
+        dateInputFormat: 'DD/MM/YYYY'
       });
-   }
+  }
 
-   togglePhotoPreview() {
+  togglePhotoPreview() {
     this.previewPhoto = !this.previewPhoto;
-   }
+  }
 
   ngOnInit() {
   }
 
-  saveEmployee(newemployee: Employee): void {
-    console.log(newemployee);
+  saveEmployee(): void {
+    // console.log(newemployee);
+    this._employeeService.save(this.employee);
+    this._router.navigate(['list']);
   }
 
 }
